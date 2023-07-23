dc2be9886e7c8f49bffd7e7bf888e9b01fdc4f18
_ Shreyas (shreyas7@deloitte.com) <shreyas7@your.hostname.com>
2023-07-23 21:39:44 +0530

    Task 4 Completed

--- controller/hackathonController.js
+++ controller/hackathonController.js
@@ -1,3 +1,4 @@
+const { HackathonEvent } = require("../model/hackatonEventSchema");
 const { addHackathonEventService } = require("../service/hackathonService");
 
 const addNewHackatonController = (req, res) => {
@@ -11,5 +12,42 @@ const addNewHackatonController = (req, res) => {
         res.status(400).json({ message: "Hackathon event hosting failed" });
       });
   };
-  
-module.exports = { addNewHackatonController };
+
+const statusListHackatonController = async(req,res) =>{
+  try {
+    const { type, page, limit } = req.query;
+
+    const currentPage = parseInt(page) || 1;
+    const itemsPerPage = parseInt(limit) || 10;
+
+    const currentDate = new Date();
+
+    const filter = {};
+    if (type === 'active') {
+      filter.startDate = { $lte: currentDate };
+      filter.endDate = { $gte: currentDate };
+    } else if (type === 'past') {
+      filter.endDate = { $lt: currentDate };
+    } else if (type === 'upcoming') {
+      filter.startDate = { $gt: currentDate };
+    }
+
+    const totalHackathons = await HackathonEvent.countDocuments(filter);
+
+    const skipItems = (currentPage - 1) * itemsPerPage;
+    const hackathons = await HackathonEvent.find(filter)
+      .skip(skipItems)
+      .limit(itemsPerPage);
+
+    res.status(200).json({
+      total: totalHackathons,
+      currentPage,
+      totalPages: Math.ceil(totalHackathons / itemsPerPage),
+      hackathons,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching hackathons', error: error.message });
+  }
+
+}
+module.exports = { addNewHackatonController,statusListHackatonController };
--- routes/hackathonRoute.js
+++ routes/hackathonRoute.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const {
   addNewHackatonController,
+  statusListHackatonController
 } = require("../controller/hackathonController");
 const hackathonRoute = express.Router();
 
 hackathonRoute.post("/addEvent", (req, res) => {
   addNewHackatonController(req, res);
+});
+
+hackathonRoute.get("/status", (req, res) => {
+  statusListHackatonController(req, res);
 });
 
 module.exports = {
